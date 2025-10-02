cfdde201c477ccb69548ed031efa033cc417c7e5
RG GLOBAL Services <team.rgglobalservices@gmail.com>
2025-10-02 21:11:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,4 +177,8 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+# For Gunicorn
+app = create_app()
